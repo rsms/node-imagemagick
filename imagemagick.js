20ba7144327a836f47799634dc7279c066b39fc7
mcfedr <mcfedr@gmail.com>
2012-12-03 14:32:38 +0200

    Fix parsing of identify
    Histogram output of identify has started to align on ':' which causes
    parseIdentify to fail for this section.

--- imagemagick.js
+++ imagemagick.js
@@ -104,7 +104,7 @@ function parseIdentify(input) {
       props = [prop],
       prevIndent = 0,
       indents = [indent],
-      currentLine, comps, indent, i;
+      currentLine, comps, indent, i, propName;
 
   lines.shift(); //drop first line (Image: name.jpg)
 
@@ -113,15 +113,17 @@ function parseIdentify(input) {
     indent = currentLine.search(/\S/);
     if (indent >= 0) {
       comps = currentLine.split(': ');
-      if (indent > prevIndent) indents.push(indent);
-      while (indent < prevIndent && props.length) {
-        indents.pop();
-        prop = props.pop();
-        prevIndent = indents[indents.length - 1];
+      if(propName != 'histogram') {
+        if (indent > prevIndent) indents.push(indent);
+        while (indent < prevIndent && props.length) {
+          indents.pop();
+          prop = props.pop();
+          prevIndent = indents[indents.length - 1];
+        }
       }
       if (comps.length < 2) {
         props.push(prop);
-        prop = prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
+        prop = prop[propName = currentLine.split(':')[0].trim().toLowerCase()] = {};
       } else {
         prop[comps[0].trim().toLowerCase()] = comps[1].trim()
       }
