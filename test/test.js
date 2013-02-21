109928d3f27328a589e1e925528224a22919c56c
Florian Sesser <fs@it-agenten.com>
2013-02-21 15:20:28 +0100

    Modify test.js for Mocha test runner

--- test/test.js
+++ test/test.js
@@ -1,52 +1,92 @@
 var fs = require('fs'),
-    im = require('../imagemagick');
+    im = require('../imagemagick'),
+    assert = require('assert');
 
 var path = __dirname+'/sample-images/blue-bottle-coffee.jpg';
 var imdata = fs.readFileSync(path, 'binary');
 
-im.identify(path, function (err, features){
-  if (err) return console.error(err.stack || err);
-  console.log('identify(path) ->', features);
-})
 
-im.identify({data:imdata}, function (err, features){
-  if (err) return console.error(err.stack || err);
-  console.log('identify({data:imdata}) ->', features);
-})
+describe('identify()', function() {
 
-im.readMetadata(path, function (err, metadata){
-  if (err) return console.error(err.stack || err);
-  console.log('readMetadata(path) ->', metadata);
-})
+  it('should return info when called with image path', function(done) {
+    im.identify(path, function (err, features){
+      assert.ifError(err);
+      assert.strictEqual(features.format, 'JPEG');
+      assert.strictEqual(features.geometry, '640x480');
+      // console.log('identify(path) ->', features);
+      done();
+    });
+  });
 
-im.readMetadata({data:imdata}, function (err, metadata){
-  if (err) return console.error(err.stack || err);
-  console.log('readMetadata({data:imdata} ->', metadata);
-})
+  it('should return info when called with object: {data: <raw image data>}', function(done) {
+    im.identify({data:imdata}, function (err, features){
+      if (err) return console.error(err.stack || err);
+        assert.ifError(err);
+        assert.strictEqual(features.format, 'JPEG');
+        assert.strictEqual(features.geometry, '640x480');
+        //console.log('identify({data:imdata}) ->', features);
+        done();
+    });
+  });
 
-var timeStarted = new Date;
-im.resize({
-  srcPath: path,
-  dstPath: 'test-resized.jpg',
-  width: 256
-}, function (err, stdout, stderr){
-  if (err) return console.error(err.stack || err);
-  console.log('resize(...) wrote "test-resized.jpg"');
-  console.log('real time taken for convert: '+((new Date)-timeStarted)+' ms');
-  im.identify(['-format', '%b', 'test-resized.jpg'], function (err, r){
-    if (err) throw err;
-    console.log("identify(['-format', '%b', 'test-resized.jpg']) ->", r);
-  })
-})
+});
 
-timeStarted = new Date;
-im.resize({
-  srcData: imdata,
-  width: 256
-}, function (err, stdout, stderr){
-  if (err) return console.error(err.stack || err);
-  console.log('real time taken for convert (with buffers): '+
-              ((new Date)-timeStarted)+' ms');
-  fs.writeFileSync('test-resized-io.jpg', stdout, 'binary');
-  console.log('resize(...) wrote "test-resized.jpg" ('+stdout.length+' Bytes)');
-})
+
+describe('readMetadata()', function() {
+
+  it('should return info when called with image path', function(done) {
+    im.readMetadata(path, function (err, metadata){
+      assert.ifError(err);
+      assert(metadata);
+      // console.log('readMetadata(path) ->', metadata);
+      done();
+    });
+  });
+
+  it('should return info when called with object: {data: <raw image data>}', function(done) {
+    im.readMetadata({data:imdata}, function (err, metadata){
+      assert.ifError(err);
+      assert(metadata);
+      // console.log('readMetadata({data:imdata} ->', metadata);
+      done();
+    });
+  });
+  
+});
+
+
+describe('resize()', function() {
+
+  it('should resize the image (by file name) and not return an error', function (done) {
+    var outFile = 'test-resized.jpg';
+    im.resize({
+      srcPath: path,
+      dstPath: outFile,
+      width: 256
+    }, function (err, stdout, stderr){
+      assert.ifError(err);
+      // console.log('resize(...) wrote "test-resized.jpg"');
+      im.identify(['-format', '%b', outFile], function (err, r){
+        assert.ifError(err);
+        assert(r, "Identify should report the file size");
+        // console.log("identify(['-format', '%b', 'test-resized.jpg']) ->", r);
+        done();
+      });
+    });
+  });
+
+  it('should resize the image (using raw data) and not return an error', function (done) {
+    var outFile = 'test-resized-io.jpg';
+    im.resize({
+      srcData: imdata,
+      width: 256
+    }, function (err, stdout, stderr){
+      assert.ifError(err);
+      fs.writeFileSync(outFile, stdout, 'binary');
+      // console.log('resize(...) wrote "test-resized-io.jpg" ('+stdout.length+' Bytes)');
+      done();
+    });
+  });
+
+});
+
