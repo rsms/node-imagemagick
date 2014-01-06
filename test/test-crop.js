4234168a1380dcb351f6a9abbdd78bb74b6f210b
Marc d'Entremont <cramhead@gmail.com>
2014-01-06 15:34:07 -0800

    Adding mocha and should for testing. Restructuring file layout for easier testing. Adjust package.json for other testers

--- test/test-crop.js
+++ test/test-crop.js
@@ -1,5 +1,5 @@
 var fs = require('fs'),
-    im = require('./imagemagick');
+    im = require('../imagemagick');
 
 var path = __dirname+'/sample-images/flynn_pentax_645D.jpg';
 var targetDirectory = __dirname + '/test-results/';
