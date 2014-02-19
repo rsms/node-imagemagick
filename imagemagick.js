3aa795d4c07d136f1fd4dd6f7b76824639bcdbc7
acardinale <a.cardinale80@gmail.com>
2014-02-19 20:06:01 +0100

    Update imagemagick.js

--- imagemagick.js
+++ imagemagick.js
@@ -122,7 +122,7 @@ function parseIdentify(input) {
       if (comps.length < 2) {
         props.push(prop);
         if(currentLine.indexOf(':')>=0)
-            prop = prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
+            prop[currentLine.split(':')[0].trim().toLowerCase()] = {};
       } else {
         prop[comps[0].trim().toLowerCase()] = comps[1].trim()
       }
